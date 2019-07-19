e04e68489c3ab914e9948998144668bc48059f02
Matthew Hackney <matthew@taomgt.com>
2019-07-19 17:14:13 -0400

    Node Update

--- StripeHttpTrigger/index.js
+++ StripeHttpTrigger/index.js
@@ -1,24 +1,59 @@
 module.exports = async function (context, req) {
-    // Using Express
+   // Using Express
 const express = require('express');
 const app = express();
-const stripe = require('stripe')('sk_test_308o9TyRPYOeaQqSI3GIwmDA');
 app.use(express.json());
 
-    // STEP 1 - Create a PaymentIntent on the server
-    (async () => {
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: 75,
+app.post('/ajax/confirm_payment', async (request, response) => {
+  try {
+    let intent;
+    if (request.body.payment_method_id) {
+      // Create the PaymentIntent
+      intent = await stripe.paymentIntents.create({
+        payment_method: request.body.payment_method_id,
+        amount: 1099,
         currency: 'usd',
-    });
-    })();
+        confirmation_method: 'manual',
+        confirm: true
+      });
+    } else if (request.body.payment_intent_id) {
+      intent = await stripe.paymentIntents.confirm(
+        request.body.payment_intent_id
+      );
+    }
+    // Send the response to the client
+    response.send(generate_payment_response(intent));
+  } catch (e) {
+    // Display error on client
+    return response.send({ error: e.message });
+  }
+});
 
-    // Step 5: Asynchronously fulfill the customer’s order
-    (async () => {
-    const intent = await stripe.paymentIntents.retrieve('pi_Aabcxyz01aDfoo');
-    const charges = intent.charges.data;
-    })();
-
+const generate_payment_response = (intent) => {
+  // Note that if your API version is before 2019-02-11, 'requires_action'
+  // appears as 'requires_source_action'.
+  if (
+    intent.status === 'requires_action' &&
+    intent.next_action.type === 'use_stripe_sdk'
+  ) {
+    // Tell the client to handle the action
+    return {
+      requires_action: true,
+      payment_intent_client_secret: intent.client_secret
+    };
+  } else if (intent.status === 'succeeded') {
+    // The payment didn’t need any additional actions and completed!
+    // Handle post-payment fulfillment
+    return {
+      success: true
+    };
+  } else {
+    // Invalid status
+    return {
+      error: 'Invalid PaymentIntent status'
+    }
+  }
+};
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
